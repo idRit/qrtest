53e46c413787cd0457abadce84a8e1e3db7fdb1a
Ritwik Mukherjee <mritwik369@gmail.com>
2020-01-17 16:14:06 +0530

    boom

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+    res.sendFile(__dirname + '/public/');
+});
+
+
+
+app.listen(3000);
